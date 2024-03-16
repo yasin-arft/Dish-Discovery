eff883b4c6c2097f1656f931dfd8f574af851833
yasin-arft <yasinarftsocial@gmail.com>
2024-03-16 12:38:58 +0600

    nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './Components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-lexend text-green'>Recipe Calories</h1>
+      <Header />
     </>
   )
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,46 @@
+import { LuUserCircle2 } from "react-icons/lu";
+import { CiSearch } from "react-icons/ci";
+
+const Header = () => {
+  return (
+    <header className="container mx-auto p-4">
+      <nav className="text-dark2 font-lexend">
+        <div className="navbar bg-base-100">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+              </div>
+              <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <li><a>Home</a></li>
+                <li><a>Recipes</a></li>
+                <li><a>About</a></li>
+                <li><a>Search</a></li>
+              </ul>
+            </div>
+            <a className="btn btn-ghost font-bold text-3xl text-dark">Recipe Calories</a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 opacity-70">
+              <li><a>Home</a></li>
+              <li><a>Recipes</a></li>
+              <li><a>About</a></li>
+              <li><a>Search</a></li>
+            </ul>
+          </div>
+          <div className="navbar-end">
+            <div className="flex items-center gap-3 mr-4">
+              <CiSearch size={24} color={'#282828b3'} />
+              <input type="text" placeholder="Search" className="input w-full max-w-xs" />
+            </div>
+            <a className="btn bg-green rounded-full h-auto p-3 shrink">
+              <LuUserCircle2 size={24} />
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
